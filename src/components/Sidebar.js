6075fd71800fb2ba4088f142fd46d8a8ef7773ae
Joe Estrem <estremjoe@gmail.com>
2024-09-27 15:17:27 +0900

    set up page switching from navbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,14 @@
 import "./Sidebar.css";
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Sidebar() {
   return (
     // I would like a search bar on top and according style pulldowns
     <div className="sidenav">
-      <p>Buttons</p>
-      <p>Cards</p>
-      <p>Alerts</p>
+     <p><Link to="/buttons">Buttons</Link></p>
+     <p><Link to="/cards">Cards</Link></p>
     </div>
   );
 }
